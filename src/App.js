c1b5a84268da3c12ae5e64beec9ec49a7d767823
Najiya-Haris <najiyaharis95@gmail.com>
2023-08-11 09:23:00 +0530

    latest commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const[toDos,setToDos]=useState([])
+  const [toDo,setToDo]=useState('')
+  const handleToDo = ()=>{
+    if(toDo.trim() === "" || toDos.some((item)=>item.text === toDo.trim())){
+      return;
+    }
+    setToDos([...toDos,{id:Date.now(),text:toDo,status:false}])
+    setToDo('')
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input value={toDo} onChange={(e)=>setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={handleToDo}className="fas fa-plus"></i>
+      </div>
+      <div className="todos">
+        {toDos.map((obj)=>{
+        return(<div className="todo">
+          <div className="left">
+            <input onChange={(e)=>{
+              console.log(e.target.checked);
+              console.log(obj);
+              setToDos(toDos.filter(obj2=>{
+                if(obj2.id===obj.id){
+                  obj2.status=e.target.checked
+                }
+                return obj2
+              }))
+            }}value={obj.status} type="checkbox" name="" id="" />
+            <p className={obj.status ? 'text-mark' : ''}>{obj.text}</p>
+            <h1>Active Task</h1>
+          </div>
+          <div className="right">
+            <i onClick={()=>setToDos(toDos.filter((newObj)=>newObj.id !== obj.id))} className="fas fa-times"></i>
+          </div>
+       
+        </div>)
+ } )}
+ {toDos.map((obj)=>{
+  if(obj.status){
+    console.log(obj.status);
+    return(<h2>{obj.text}</h2>)
+  }
+return null
+ })}
+      </div>
+    </div>
+
+
+    
     </div>
   );
 }
